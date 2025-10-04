a511c707cdef12d0cde1e0e799018d166bacf656
KhanhDepTrai <Khanhtqph42311@gmail.com>
2025-10-04 21:06:05 +0700

    delete all class irrelevant

--- src/main/java/com/example/backend/exception/ResourceAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.exception;
-
-public class ResourceAlreadyExistsException extends RuntimeException {
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-}
